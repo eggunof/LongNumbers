b909017ffef19846015ff6e8bebc0ca14590d181
Varfolomeeva Arina <141402789+varfik@users.noreply.github.com>
2024-11-03 21:28:27 +0300

    Update DIV_NN_Dk.cc

--- src/modules/N.cc
+++ src/modules/N.cc
@@ -37,8 +37,27 @@ N SUB_NDN_N(N a, N b, Digit d) {
   return N();
 }
 
-Digit DIV_NN_Dk(N a, N b, uint32_t k) {
-  return 0;
+// Вычисление первой цифры деления большего натурального на меньшее, домноженное на 10^k,где k - номер позиции этой цифры
+Digit DIV_NN_Dk(N a, N b) {
+    uint32_t k; // это позиция первой значащей цифры частного (считая от старшего разряда)
+    N big;
+    N small;
+    if (COM_NN_D(a, b) == 2) { // a больше b
+        big = a;
+        small = b;
+    }
+    else { // a меньше или равно b
+        big = b;
+        small = a;
+    }
+    k = big.size() - small.size(); // вычисление номера позиции первой цифры при делении натуральных
+    small = MUL_Nk_N(small, k); // умножение меньшего натурального числа на 10^k
+    Digit firstDigit = 0;
+    while (COM_NN_D(big, small) == 2 || COM_NN_D(big, small) == 0) { // Пока большее натуральное больше или равно меньшему
+        big = SUB_NN_N(big, small); // вычитание меньшего из большего
+        firstDigit++; // увеличивание первой цифры частного
+    }
+    return firstDigit; // возвращение первой цифры частного
 }
 
 N DIV_NN_N(N a, N b) {
