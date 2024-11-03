5f3aa1582f9f04883c37356c890b5b0eafda3ad3
eggunof <eggunof@gmail.com>
2024-11-03 17:29:13 +0300

    remove const references

--- src/modules/Z.cc
+++ src/modules/Z.cc
@@ -1,42 +1,42 @@
 
 #include "Z.h"
 
-N ABS_Z_N(const N &a) {
+N ABS_Z_N(N a) {
   return N();
 }
 
-uint8_t POZ_Z_D(const Z &a) {
+uint8_t POZ_Z_D(Z a) {
   return 0;
 }
 
-Z MUL_ZM_Z(const Z &a) {
+Z MUL_ZM_Z(Z a) {
   return Z();
 }
 
-Z TRANS_N_Z(const N &a) {
+Z TRANS_N_Z(N a) {
   return Z();
 }
 
-N TRANS_Z_N(const Z &a) {
+N TRANS_Z_N(Z a) {
   return N();
 }
 
-Z ADD_ZZ_Z(const Z &a, const Z &b) {
+Z ADD_ZZ_Z(Z a, Z b) {
   return Z();
 }
 
-Z SUB_ZZ_Z(const Z &a, const Z &b) {
+Z SUB_ZZ_Z(Z a, Z b) {
   return Z();
 }
 
-Z MUL_ZZ_Z(const Z &a, const Z &b) {
+Z MUL_ZZ_Z(Z a, Z b) {
   return Z();
 }
 
-Z DIV_ZZ_Z(const Z &a, const Z &b) {
+Z DIV_ZZ_Z(Z a, Z b) {
   return Z();
 }
 
-Z MOD_ZZ_Z(const Z &a, const Z &b) {
+Z MOD_ZZ_Z(Z a, Z b) {
   return Z();
 }
--- src/modules/Z.h
+++ src/modules/Z.h
@@ -9,24 +9,24 @@ struct Z {
   bool sign;
 };
 
-N ABS_Z_N(const N &a);
+N ABS_Z_N(N a);
 
-uint8_t POZ_Z_D(const Z &a);
+uint8_t POZ_Z_D(Z a);
 
-Z MUL_ZM_Z(const Z &a);
+Z MUL_ZM_Z(Z a);
 
-Z TRANS_N_Z(const N &a);
+Z TRANS_N_Z(N a);
 
-N TRANS_Z_N(const Z &a);
+N TRANS_Z_N(Z a);
 
-Z ADD_ZZ_Z(const Z &a, const Z &b);
+Z ADD_ZZ_Z(Z a, Z b);
 
-Z SUB_ZZ_Z(const Z &a, const Z &b);
+Z SUB_ZZ_Z(Z a, Z b);
 
-Z MUL_ZZ_Z(const Z &a, const Z &b);
+Z MUL_ZZ_Z(Z a, Z b);
 
-Z DIV_ZZ_Z(const Z &a, const Z &b);
+Z DIV_ZZ_Z(Z a, Z b);
 
-Z MOD_ZZ_Z(const Z &a, const Z &b);
+Z MOD_ZZ_Z(Z a, Z b);
 
 #endif //LONGNUMBERS_SRC_MODULES_Z_H_
