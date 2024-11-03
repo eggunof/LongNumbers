9d1eaa3c3293152e44aa2a9e95b73e62e37c5bd0
eggunof <eggunof@gmail.com>
2024-11-03 17:21:29 +0300

    remove const references

--- src/modules/N.cc
+++ src/modules/N.cc
@@ -1,58 +1,58 @@
 
 #include "N.h"
 
-uint8_t COM_NN_D(const N &a, const N &b) {
+uint8_t COM_NN_D(N a, N b) {
   return 0;
 }
 
-bool NZER_N_B(const N &a) {
+bool NZER_N_B(N a) {
   return false;
 }
 
-N ADD_1N_N(const N &a) {
+N ADD_1N_N(N a) {
   return N();
 }
 
-N ADD_NN_N(const N &a, const N &b) {
+N ADD_NN_N(N a, N b) {
   return N();
 }
 
-N SUB_NN_N(const N &a, const N &b) {
+N SUB_NN_N(N a, N b) {
   return N();
 }
 
-N MUL_ND_N(const N &a, const Digit &d) {
+N MUL_ND_N(N a, Digit d) {
   return N();
 }
 
-N MUL_Nk_N(const N &a, const uint32_t &k) {
+N MUL_Nk_N(N a, uint32_t k) {
   return N();
 }
 
-N MUL_NN_N(const N &a, const N &b) {
+N MUL_NN_N(N a, N b) {
   return N();
 }
 
-N SUB_NDN_N(const N &a, const N &b, const Digit &d) {
+N SUB_NDN_N(N a, N b, Digit d) {
   return N();
 }
 
-Digit DIV_NN_Dk(const N &a, const N &b, const uint32_t &k) {
+Digit DIV_NN_Dk(N a, N b, uint32_t k) {
   return 0;
 }
 
-N DIV_NN_N(const N &a, const N &b) {
+N DIV_NN_N(N a, N b) {
   return N();
 }
 
-N MOD_NN_N(const N &a, const N &b) {
+N MOD_NN_N(N a, N b) {
   return N();
 }
 
-N GCF_NN_N(const N &a, const N &b) {
+N GCF_NN_N(N a, N b) {
   return N();
 }
 
-N LCM_NN_N(const N &a, const N &b) {
+N LCM_NN_N(N a, N b) {
   return N();
 }
--- src/modules/N.h
+++ src/modules/N.h
@@ -8,32 +8,32 @@
 typedef uint8_t Digit;
 typedef std::vector<Digit> N;
 
-uint8_t COM_NN_D(const N &a, const N &b);
+uint8_t COM_NN_D(N a, N b);
 
-bool NZER_N_B(const N &a);
+bool NZER_N_B(N a);
 
-N ADD_1N_N(const N &a);
+N ADD_1N_N(N a);
 
-N ADD_NN_N(const N &a, const N &b);
+N ADD_NN_N(N a, N b);
 
-N SUB_NN_N(const N &a, const N &b);
+N SUB_NN_N(N a, N b);
 
-N MUL_ND_N(const N &a, const Digit &d);
+N MUL_ND_N(N a, Digit d);
 
-N MUL_Nk_N(const N &a, const uint32_t &k);
+N MUL_Nk_N(N a, uint32_t k);
 
-N MUL_NN_N(const N &a, const N &b);
+N MUL_NN_N(N a, N b);
 
-N SUB_NDN_N(const N &a, const N &b, const Digit &d);
+N SUB_NDN_N(N a, N b, Digit d);
 
-Digit DIV_NN_Dk(const N &a, const N &b, const uint32_t &k);
+Digit DIV_NN_Dk(N a, N b, uint32_t k);
 
-N DIV_NN_N(const N &a, const N &b);
+N DIV_NN_N(N a, N b);
 
-N MOD_NN_N(const N &a, const N &b);
+N MOD_NN_N(N a, N b);
 
-N GCF_NN_N(const N &a, const N &b);
+N GCF_NN_N(N a, N b);
 
-N LCM_NN_N(const N &a, const N &b);
+N LCM_NN_N(N a, N b);
 
 #endif //LONGNUMBERS_SRC_MODULES_N_H_
