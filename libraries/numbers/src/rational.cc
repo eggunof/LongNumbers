a89573f263da25a56f154dc288b61a32face5f26
eggunof <eggunof@gmail.com>
2024-11-09 20:18:18 +0300

    resolve conflict

--- libraries/numbers/src/rational.cc
+++ libraries/numbers/src/rational.cc
@@ -44,7 +44,23 @@ Rational Rational::operator/(const Rational &rhs) const { return {}; }
 
 Rational Rational::operator+=(const Rational &rhs) { return {}; }
 
-Rational Rational::operator-=(const Rational &rhs) { return {}; }
+Rational Rational::operator-=(const Rational &rhs) {
+  /// Находим общий знаменатель
+  Natural Denum = this->denominator_.LCM_NN_N(other.denominator_);
+
+  // Находим числители с учетом дополнительных множителей
+  Integer Num1 =
+      this->numerator_.MUL_ZZ_Z(Integer(Denum.DIV_NN_N(this->denominator_)));
+  Integer Num2 =
+      other.numerator_.MUL_ZZ_Z(Integer(Denum.DIV_NN_N(other.denominator_)));
+
+  // Вычитаем числитель второй дроби из первой
+  Integer Num = Num1.SUB_ZZ_Z(Num2);
+
+  // Создаем результат
+  Rational result(Num, Denum);
+  return result;
+}
 
 Rational Rational::operator*=(const Rational &rhs) { return {}; }
 
