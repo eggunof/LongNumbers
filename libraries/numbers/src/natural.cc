a9e210f103904fa6dd95bc76c3b3e9df4dac65ed
eggunof <eggunof@gmail.com>
2024-11-09 19:02:44 +0300

    reformat code

--- libraries/numbers/src/natural.cc
+++ libraries/numbers/src/natural.cc
@@ -24,13 +24,15 @@ Natural::Natural(const std::vector<Digit> &digits) {
 
 Natural::Natural(const std::string &string) {
   if (string.empty()) {
-    throw std::invalid_argument("Invalid input: string must contain at least one digit");
+    throw std::invalid_argument(
+        "Invalid input: string must contain at least one digit");
   }
   digits_.reserve(string.length());
   for (char digit : string) {
     Digit x = digit - '0';
     if (x >= 10) {
-      throw std::invalid_argument("Invalid input: non-digit character in string");
+      throw std::invalid_argument(
+          "Invalid input: non-digit character in string");
     }
     if (!digits_.empty() || x != 0) {
       digits_.push_back(x);
@@ -46,106 +48,64 @@ Comparison Natural::Compare(const Natural &first, const Natural &second) {
   return Comparison::EQUAL;
 }
 
-bool Natural::operator==(const Natural &rhs) const {
-  return false;
-}
+bool Natural::operator==(const Natural &rhs) const { return false; }
 
-bool Natural::operator!=(const Natural &rhs) const {
-  return false;
-}
+bool Natural::operator!=(const Natural &rhs) const { return false; }
 
-bool Natural::operator<(const Natural &rhs) const {
-  return false;
-}
+bool Natural::operator<(const Natural &rhs) const { return false; }
 
-bool Natural::operator>(const Natural &rhs) const {
-  return false;
-}
+bool Natural::operator>(const Natural &rhs) const { return false; }
 
-bool Natural::operator<=(const Natural &rhs) const {
-  return false;
-}
+bool Natural::operator<=(const Natural &rhs) const { return false; }
 
-bool Natural::operator>=(const Natural &rhs) const {
-  return false;
-}
+bool Natural::operator>=(const Natural &rhs) const { return false; }
 
-bool Natural::IsZero() const {
-  return digits_.size() == 1 && digits_[0] == 0;
-}
+bool Natural::IsZero() const { return digits_.size() == 1 && digits_[0] == 0; }
 
-Natural &Natural::operator++() {
-  return *this;
-}
+Natural &Natural::operator++() { return *this; }
 
-Natural &Natural::operator++(int) {
-  return *this;
-}
+Natural &Natural::operator++(int) { return *this; }
 
-Natural Natural::operator+(const Natural &rhs) const {
-  return {};
-}
+Natural Natural::operator+(const Natural &rhs) const { return {}; }
 
-Natural Natural::operator-(const Natural &rhs) const {
-  return {};
-}
+Natural Natural::operator-(const Natural &rhs) const { return {}; }
 
-Natural Natural::operator*(Digit d) const {
-  return {};
-}
+Natural Natural::operator*(Digit d) const { return {}; }
 
-Natural Natural::operator*(const Natural &rhs) const {
-  return {};
-}
+Natural Natural::operator*(const Natural &rhs) const { return {}; }
 
-Natural Natural::operator/(const Natural &rhs) const {
-  return {};
-}
+Natural Natural::operator/(const Natural &rhs) const { return {}; }
 
-Natural Natural::operator%(const Natural &rhs) const {
-  return {};
-}
+Natural Natural::operator%(const Natural &rhs) const { return {}; }
 
-Natural &Natural::operator+=(const Natural &rhs) {
-  return *this;
-}
+Natural &Natural::operator+=(const Natural &rhs) { return *this; }
 
-Natural &Natural::operator-=(const Natural &rhs) {
-  return *this;
-}
+Natural &Natural::operator-=(const Natural &rhs) { return *this; }
 
-Natural &Natural::operator*=(Digit d) {
-  return *this;
-}
+Natural &Natural::operator*=(Digit d) { return *this; }
 
-Natural &Natural::operator*=(const Natural &rhs) {
-  return *this;
-}
+Natural &Natural::operator*=(const Natural &rhs) { return *this; }
 
-Natural &Natural::operator/=(const Natural &rhs) {
-  return *this;
-}
+Natural &Natural::operator/=(const Natural &rhs) { return *this; }
 
-Natural &Natural::operator%=(const Natural &rhs) {
-  return *this;
-}
+Natural &Natural::operator%=(const Natural &rhs) { return *this; }
 
 Natural &Natural::SubtractMultiplied(const Natural &rhs, Digit d) {
   return *this;
 }
 
-Natural &Natural::MultiplyBy10Power(uint32_t k) {
-  return *this;
-}
+Natural &Natural::MultiplyBy10Power(uint32_t k) { return *this; }
 
 Digit Natural::GetLeadingDigitAfterDivision(const Natural &rhs, uint32_t k) {
   return {};
 }
 
-Natural Natural::GreatestCommonDivisor(const Natural &first, const Natural &second) {
+Natural Natural::GreatestCommonDivisor(const Natural &first,
+                                       const Natural &second) {
   return {};
 }
 
-Natural Natural::LeastCommonMultiple(const Natural &first, const Natural &second) {
+Natural Natural::LeastCommonMultiple(const Natural &first,
+                                     const Natural &second) {
   return {};
 }
