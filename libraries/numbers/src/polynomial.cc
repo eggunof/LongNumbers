d70c7b9bc1c2d66fe337bb948518bbf5ea2d6136
eggunof <eggunof@gmail.com>
2024-11-09 20:15:08 +0300

    resolve conflict

--- libraries/numbers/src/polynomial.cc
+++ libraries/numbers/src/polynomial.cc
@@ -17,7 +17,17 @@ Polynomial &Polynomial::operator+=(const Polynomial &rhs) { return *this; }
 
 Polynomial &Polynomial::operator-=(const Polynomial &rhs) { return *this; }
 
-Polynomial &Polynomial::operator*=(const Rational &scalar) { return *this; }
+Polynomial &Polynomial::operator*=(const Rational &scalar) {
+  std::map<Natural, Rational, Comparator> temp = this->coefficients_;
+  // Пройдемся по коэффициентам многочлена домножая каждый на рациональное число
+  for (auto it = temp.begin(); it != temp.end(); it++)
+    temp[it->first] = (temp[it->first]) *= (scalar);
+
+  Polynomial *MUL = new Polynomial(temp);
+
+  return *MUL;
+  return *this;
+}
 
 Polynomial &Polynomial::operator*=(const Polynomial &rhs) { return *this; }
 
