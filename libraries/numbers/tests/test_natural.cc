c683f443112d004e371155a61d159a8d46241991
Daniil Egunov <87710548+eggunof@users.noreply.github.com>
2024-11-10 18:10:06 +0300

    N-8 (#50)

--- libraries/numbers/tests/test_natural.cc
+++ libraries/numbers/tests/test_natural.cc
@@ -38,16 +38,40 @@ TEST(NaturalTest, Addition) {
 
 TEST(NaturalTest, MultiplyByDigit) {
   Natural a("9999999");
-  EXPECT_EQ(a *= 9, Natural("89999991"));
+  a *= 9;
+  EXPECT_EQ(a, Natural("89999991"));
 
   Natural b("5473");
-  EXPECT_EQ(b *= 0, Natural("0"));
+  b *= 0;
+  EXPECT_EQ(b, Natural("0"));
 
   Natural c("123456789");
-  EXPECT_EQ(c *= 1, Natural("123456789"));
+  c *= 1;
+  EXPECT_EQ(c, Natural("123456789"));
 
   Natural d("123321");
   EXPECT_EQ(d * 7, Natural("863247"));
+}
+
+TEST(NaturalTest, Multiply) {
+  Natural a("123456789");
+  Natural b("987654321");
+  a *= b;
+  EXPECT_EQ(a, Natural("121932631112635269"));
+
+  Natural c("1234567890");
+  Natural d("0");
+  c *= d;
+  EXPECT_EQ(c, Natural("0"));
+
+  Natural e("0");
+  Natural f("9876543210");
+  e *= f;
+  EXPECT_EQ(e, Natural("0"));
+
+  Natural g("15468");
+  Natural h("98456159884415");
+  EXPECT_EQ(g * h, Natural("1522919881092131220"));
 }
 
 TEST(NaturalTest, MultiplyBy10Power) {
