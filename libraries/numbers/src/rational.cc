5bf321e07ca03dc42c4f92e843a78f25e77ac312
veronika14may <144445023+veronika14may@users.noreply.github.com>
2024-11-10 21:59:32 +0300

    Update rational.cc Q-5

--- libraries/numbers/src/rational.cc
+++ libraries/numbers/src/rational.cc
@@ -34,7 +34,21 @@ bool Rational::IsInteger() {
   return false;
 }
 
-Rational Rational::operator+(const Rational &rhs) const { return {}; }
+// Сложение дробей "+"
+// Над модулем работала Майская Вероника, гр. 3384
+Rational Rational::operator+(const Rational &rhs) const {
+    // Находим НОК знаменателей
+    Natural common_denominator = LCM_NN_N(this->denominator_, rhs.denominator_);
+    // Определяем множители для каждого числителя
+    Natural scale_left = common_denominator / this->denominator_;
+    Natural scale_right = common_denominator / rhs.denominator_;
+    // Приводим числители к общему знаменателю
+    Integer new_numerator_left = MUL_ZZ_Z(this->numerator_, Integer(scale_left));
+    Integer new_numerator_right = MUL_ZZ_Z(this->numerator_, Integer(scale_right));
+    // Складываем числители
+    Integer result_numerator = ADD_ZZ_Z(new_numerator_left, new_numerator_right);
+    return Rational(result_numerator, common_denominator);
+}
 
 Rational Rational::operator-(const Rational &rhs) const { return {}; }
 
@@ -42,7 +56,12 @@ Rational Rational::operator*(const Rational &rhs) const { return {}; }
 
 Rational Rational::operator/(const Rational &rhs) const { return {}; }
 
-Rational Rational::operator+=(const Rational &rhs) { return {}; }
+// Сложение дробей "+="
+// Над модулем работала Майская Вероника, гр. 3384
+Rational Rational::operator+=(const Rational &rhs) {
+    *this = *this + rhs;
+    return *this;
+}
 
 Rational Rational::operator-=(const Rational &rhs) { return {}; }
 
