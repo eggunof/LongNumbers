27c0c5986a4752f95c6678e18d8e9ea663d294fa
Varfolomeeva Arina <141402789+varfik@users.noreply.github.com>
2024-11-10 13:36:42 +0300

    Update test_natural.cc ADD_NN_N
    fixed test

--- libraries/numbers/tests/test_natural.cc
+++ libraries/numbers/tests/test_natural.cc
@@ -9,8 +9,8 @@ TEST(NaturalTest, CreateEmpty) {
 }
 
 TEST(NaturalTest, ADD_NN_N) {
-  Natutal a(10099);
-  Natural b(599999);
+  Natutal a("10099");
+  Natural b("599999");
   a += b;
-  EXPECT_EQ(a, Natural(610098));
+  EXPECT_EQ(a, Natural("610098"));
 }
