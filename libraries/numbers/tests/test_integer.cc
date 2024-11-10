aab42d7c568416fc37302a1d99036c72954f3f48
veronika14may <144445023+veronika14may@users.noreply.github.com>
2024-11-10 19:17:22 +0300

    Update test_integer.cc Z-8

--- libraries/numbers/tests/test_integer.cc
+++ libraries/numbers/tests/test_integer.cc
@@ -16,3 +16,22 @@ TEST(IntegerTest, Sign) {
   Integer negative_zero_integer({0}, Sign::NEGATIVE);
   EXPECT_EQ(negative_zero_integer.GetSign(), Sign::ZERO);
 }
+
+TEST(IntegerTest, Multiplication) {
+  Integer a({1, 2, 3}, Sign::POSITIVE);
+  Integer b({4, 5, 6}, Sign::POSITIVE);
+  Integer expected_positive({5, 6, 0, 8, 8}, Sign::POSITIVE);
+  EXPECT_EQ(a * b, expected_positive);
+
+  Integer expected_negative({5, 6, 0, 8, 8}, Sign::NEGATIVE);
+  EXPECT_EQ(a * Integer({4, 5, 6}, Sign::NEGATIVE), expected_negative);
+
+  EXPECT_EQ(Integer({1, 2, 3}, Sign::NEGATIVE) * Integer({4, 5, 6}, Sign::NEGATIVE), expected_positive);
+
+  Integer zero({0}, Sign::ZERO);
+  Integer expected_zero({0}, Sign::ZERO);
+  EXPECT_EQ(a * zero, expected_zero);
+  EXPECT_EQ(zero * a, expected_zero);
+
+  EXPECT_EQ(zero * zero, expected_zero);
+}
