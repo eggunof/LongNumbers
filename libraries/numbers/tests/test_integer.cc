34ef7550daff34b93ee5fa6244e937e25f316454
eggunof <eggunof@gmail.com>
2024-11-10 22:39:17 +0300

    tests

--- libraries/numbers/tests/test_integer.cc
+++ libraries/numbers/tests/test_integer.cc
@@ -10,28 +10,24 @@ TEST(IntegerTest, Sign) {
   Integer negative_integer({1, 2, 3}, Sign::NEGATIVE);
   EXPECT_EQ(negative_integer.GetSign(), Sign::NEGATIVE);
 
-  Integer zero_integer({0}, Sign::ZERO);
+  Integer zero_integer({0, 0, 0}, Sign::ZERO);
   EXPECT_EQ(zero_integer.GetSign(), Sign::ZERO);
 
-  Integer negative_zero_integer({0}, Sign::NEGATIVE);
+  Integer negative_zero_integer({0, 0}, Sign::NEGATIVE);
   EXPECT_EQ(negative_zero_integer.GetSign(), Sign::ZERO);
 }
 
 TEST(IntegerTest, Multiplication) {
-  Integer a({1, 2, 3}, Sign::POSITIVE);
-  Integer b({4, 5, 6}, Sign::POSITIVE);
-  Integer expected_positive({5, 6, 0, 8, 8}, Sign::POSITIVE);
-  EXPECT_EQ(a * b, expected_positive);
-
-  Integer expected_negative({5, 6, 0, 8, 8}, Sign::NEGATIVE);
-  EXPECT_EQ(a * Integer({4, 5, 6}, Sign::NEGATIVE), expected_negative);
-
-  EXPECT_EQ(Integer({1, 2, 3}, Sign::NEGATIVE) * Integer({4, 5, 6}, Sign::NEGATIVE), expected_positive);
-
-  Integer zero({0}, Sign::ZERO);
-  Integer expected_zero({0}, Sign::ZERO);
-  EXPECT_EQ(a * zero, expected_zero);
-  EXPECT_EQ(zero * a, expected_zero);
-
-  EXPECT_EQ(zero * zero, expected_zero);
+  Integer a("123");
+  Integer b("456");
+  EXPECT_EQ(a * b, Integer("56088"));
+  Integer c("-123");
+  Integer d("456");
+  EXPECT_EQ(c * d, Integer("-56088"));
+  Integer e("-123");
+  Integer f("-456");
+  EXPECT_EQ(e * f, Integer("56088"));
+  Integer g("0");
+  Integer h("456");
+  EXPECT_EQ(g * h, Integer("0"));
 }
