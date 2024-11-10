0a953cdc363ead439ad35b79b317ef1cfa3b7afd
eggunof <eggunof@gmail.com>
2024-11-10 21:35:46 +0300

    fix

--- libraries/numbers/src/natural.cc
+++ libraries/numbers/src/natural.cc
@@ -70,9 +70,7 @@ bool Natural::operator<(const Natural &rhs) const {
   return Compare(*this, rhs) == Comparison::LESS;
 }
 
-bool Natural::operator>(const Natural &rhs) const {
-  return Compare(*this, rhs) == Comparison::GREATER;
-}
+bool Natural::operator>(const Natural &rhs) const { return rhs < *this; }
 
 bool Natural::operator<=(const Natural &rhs) const { return !(*this > rhs); }
 
