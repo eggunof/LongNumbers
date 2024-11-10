adc7994a08fdb48cbde93532fd51bbd249af8bbe
veronika14may <144445023+veronika14may@users.noreply.github.com>
2024-11-10 14:32:06 +0300

    N-1 (#42)
    N-1
    
    ---------
    
    Co-authored-by: eggunof <eggunof@gmail.com>

--- libraries/numbers/tests/test_natural.cc
+++ libraries/numbers/tests/test_natural.cc
@@ -7,3 +7,15 @@ TEST(NaturalTest, CreateEmpty) {
   Natural natural;
   EXPECT_TRUE(natural.IsZero());
 }
+
+TEST(NaturalTest, Compare) {
+  Natural first("5246345451");
+  Natural second("5263345651");
+  EXPECT_EQ(Natural::Compare(first, second), Comparison::LESS);
+  EXPECT_FALSE(first == second);
+  EXPECT_TRUE(first != second);
+  EXPECT_TRUE(first < second);
+  EXPECT_FALSE(first > second);
+  EXPECT_TRUE(first <= second);
+  EXPECT_FALSE(first >= second);
+}
