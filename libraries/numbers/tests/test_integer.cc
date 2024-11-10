7a82fa496ac345ef759ef001f6659c13c91adc66
Anna Kadnikova <97295264+hellbruh@users.noreply.github.com>
2024-11-10 23:06:02 +0300

    Z7

--- libraries/numbers/tests/test_integer.cc
+++ libraries/numbers/tests/test_integer.cc
@@ -16,3 +16,20 @@ TEST(IntegerTest, Sign) {
   Integer negative_zero_integer({0}, Sign::NEGATIVE);
   EXPECT_EQ(negative_zero_integer.GetSign(), Sign::ZERO);
 }
+
+TEST(IntegerTest, sub){
+  Integer a1({1, 2, 3}, Sign::POSITIVE);
+  Integer b1({1, 2, 3}, Sign::NEGATIVE);
+  Integer c1({2,4,6}, Sign::POSITIVE);
+  EXPECT_EQ(a1 - b1, c1);
+
+  Integer a2({1, 2, 3}, Sign::POSITIVE);
+  Integer b2({1, 2, 3}, Sign::POSITIVE);
+  Integer c2({0}, Sign::ZERO);
+  EXPECT_EQ(a2 - b2, c2);
+
+  Integer a3({1, 2, 3}, Sign::POSITIVE);
+  Integer b3({2, 4, 6}, Sign::POSITIVE);
+  Integer c3({1,2,3}, Sign::NEGATIVE);
+  EXPECT_EQ(a3 - b3, c3);
+}
