621d0c35a792416704760ebf838ed32657fcde3b
tylerdurden2005 <matveev.nikita.2005@mail.ru>
2024-11-11 00:38:30 +0300

    Q-2 tests

--- libraries/numbers/tests/test_rational.cc
+++ libraries/numbers/tests/test_rational.cc
@@ -4,3 +4,16 @@
 #include "rational.h"
 
 TEST(RationalTest, Dummy) { EXPECT_TRUE(true); }
+
+TEST(RationalTest, IsIntTest){
+    Rational rational1("1/3");
+    EXPECT_FALSE(rational1.IsInteger());
+    Rational rational2("-5322/1");
+    EXPECT_TRUE(rational2.IsInteger());
+    Rational rational3("10/2");
+    EXPECT_TRUE(rational3.IsInteger());
+    Rational rational4("17/34");
+    EXPECT_FALSE(rational4.IsInteger());
+    Rational rational5("0");
+    EXPECT_TRUE(rational5.IsInteger());
+}
