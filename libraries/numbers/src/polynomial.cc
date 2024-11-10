50b067f68a0d397edf41cb6fb959b68fe3fc1fd7
Anna Kadnikova <97295264+hellbruh@users.noreply.github.com>
2024-11-10 22:40:00 +0300

    p3

--- libraries/numbers/src/polynomial.cc
+++ libraries/numbers/src/polynomial.cc
@@ -5,7 +5,16 @@ Polynomial Polynomial::operator+(const Polynomial &rhs) const { return {}; }
 
 Polynomial Polynomial::operator-(const Polynomial &rhs) const { return {}; }
 
-Polynomial Polynomial::operator*(const Rational &scalar) const { return {}; }
+Polynomial Polynomial::operator*(const Rational &scalar) const { 
+  std::map<Natural, Rational, Comparator> temp = this->coefficients_;
+  // Пройдемся по коэффициентам многочлена домножая каждый на рациональное число
+  for (auto it = temp.begin(); it != temp.end(); it++)
+    temp[it->first] = (temp[it->first]) *= (scalar);
+
+  Polynomial *MUL = new Polynomial(temp);
+
+  return *MUL;
+  return *this; }
 
 Polynomial Polynomial::operator*(const Polynomial &rhs) const { return {}; }
 
