d77d5fe1d1e2132695662aa32a40db0d14be485a
veronika14may <144445023+veronika14may@users.noreply.github.com>
2024-11-10 23:42:27 +0300

    Update test_integer.cc Z-9

--- libraries/numbers/tests/test_integer.cc
+++ libraries/numbers/tests/test_integer.cc
@@ -31,3 +31,17 @@ TEST(IntegerTest, Multiplication) {
   Integer h("456");
   EXPECT_EQ(g * h, Integer("0"));
 }
+
+TEST(IntegerTest, Division) {
+  Integer a("12345");
+  Integer b("123");
+  EXPECT_EQ(a / b, Integer("100"));
+
+  Integer c("-12345");
+  Integer d("123");
+  EXPECT_EQ(c / d, Integer("-100"));
+
+  Integer e("0");
+  Integer f("123");
+  EXPECT_EQ(e / f, Integer("0"));
+}
