bed9e6e049882e8c03bf8a3ac616b72d65bf6f05
Darya Dmitrieva <144445820+dmitrievaaxx@users.noreply.github.com>
2024-11-12 01:31:55 +0300

    cmp

--- libraries/numbers/src/rational.cc
+++ libraries/numbers/src/rational.cc
@@ -33,6 +33,33 @@ bool Rational::operator!=(const Rational &rhs) const {
     return numerator_ != rhs.numerator_ || denominator_ != rhs.denominator_;
 }
 
+bool Rational::operator<(const Rational &rhs) const {
+    if (denominator_ == rhs.denominator_) {
+        return numerator_ < rhs.numerator_;
+    }
+    // Находим НОК для знаменателей
+    Natural lcm_denominator = Natural::LeastCommonMultiple(denominator_, rhs.denominator_);
+
+    // Приводим обе дроби к общему знаменателю
+    Integer left_numerator = numerator_ * Integer(lcm_denominator / denominator_);
+    Integer right_numerator = rhs.numerator_ * Integer(lcm_denominator / rhs.denominator_);
+
+    // Сравниваем числители
+    return left_numerator < right_numerator;
+}
+
+bool Rational::operator>(const Rational &rhs) const {
+    return rhs < *this; // Используем оператор <
+}
+
+bool Rational::operator<=(const Rational &rhs) const {
+    return !(*this > rhs); // Используем оператор >
+}
+
+bool Rational::operator>=(const Rational &rhs) const {
+    return !(*this < rhs); // Используем оператор <
+}
+
 Rational::operator Integer() const { return {}; }
 
 Rational &Rational::Reduce() { return *this; }
