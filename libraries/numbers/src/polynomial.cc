19de52ba61c4de689552c1e539db7378f6bda880
tylerdurden2005 <matveev.nikita.2005@mail.ru>
2024-11-11 20:54:08 +0300

    P-11

--- libraries/numbers/src/polynomial.cc
+++ libraries/numbers/src/polynomial.cc
@@ -31,9 +31,27 @@ Rational Polynomial::ToIntegerCoefficients(const Polynomial &polynomial) {
   return {};
 }
 
-Polynomial Polynomial::GreatestCommonDivisor(const Polynomial &first,
-                                             const Polynomial &second) {
-  return {};
+// НОД многочленов
+// Над модулем работал Матвеев Никита гр. 3383
+Polynomial Polynomial::GreatestCommonDivisor(const Polynomial &first, const Polynomial &second) {
+    Polynomial dividend; // делимое
+    Polynomial divisor; // делитель
+    Polynomial remains; // для хранения остатка
+    if (first.GetDegree() >= second.GetDegree()) { // если степень первого превосходи или равна второму
+        dividend = first; // делимое - первое число, делитель второе
+        divisor = second;
+    } else { // иначе числа меняются местами
+        dividend = second;
+        divisor = first;
+    }
+    remains = dividend % divisor; // первый раз находим остаток от деления первого на второго
+    while (!remains.coefficients_.empty()) { // пока отсаток не равен 0 (не пуст равнозначно)
+        dividend = divisor; // теперь делимое - бывший делитель
+        divisor = remains; // делитель - получившийся остаток
+        remains = dividend % divisor; // новый остаток от деления новых элементов
+    }
+    divisor.ToIntegerCoefficients(); // выносится общий множитель коэффициентов 
+    return divisor;
 }
 
 Polynomial Polynomial::Derivative(const Polynomial &polynomial) { return {}; }
--- libraries/numbers/tests/test_polynomial.cc
+++ libraries/numbers/tests/test_polynomial.cc
@@ -3,4 +3,50 @@
 
 #include "polynomial.h"
 
-TEST(PolynomialTest, Dummy) { EXPECT_TRUE(true); }
+TEST(PolynomialTest, GCDTest){
+    Polynomial pol1({
+        {Natural(0), Rational(Integer(1), Natural(1))},
+        {Natural(1), Rational(Integer(2), Natural(1))},
+        {Natural(2), Rational(Integer(1), Natural(1))}
+    });
+    Polynomial pol2({
+        {Natural(0), Rational(Integer(1), Natural(1))},
+        {Natural(1), Rational(Integer(1), Natural(1))},
+    });
+    EXPECT_EQ(Polynomial::GreatestCommonDivisor(pol1, pol2), pol2);
+    EXPECT_EQ(Polynomial::GreatestCommonDivisor(pol2, pol1), pol2);
+    Polynomial pol3({
+        {Natural(5), Rational(Integer(2), Natural(1))},
+        {Natural(4), Rational(Integer(-3), Natural(1))},
+        {Natural(3), Rational(Integer(-5), Natural(1))},
+        {Natural(2), Rational(Integer(1), Natural(1))},
+        {Natural(1), Rational(Integer(6), Natural(1))},
+        {Natural(0), Rational(Integer(3), Natural(1))}
+    });
+    Polynomial pol4({
+         {Natural(4), Rational(Integer(3), Natural(1))},
+         {Natural(3), Rational(Integer(2), Natural(1))},
+         {Natural(2), Rational(Integer(-3), Natural(1))},
+         {Natural(1), Rational(Integer(-5), Natural(1))},
+         {Natural(0), Rational(Integer(-2), Natural(1))}
+    });
+    Polynomial res({
+        {Natural(3), Rational(Integer(1), Natural(1))},
+        {Natural(1), Rational(Integer(-1), Natural(1))},
+        {Natural(0), Rational(Integer(-1), Natural(1))}
+    });
+    EXPECT_EQ(Polynomial::GreatestCommonDivisor(pol3, pol4), res);
+    EXPECT_EQ(Polynomial::GreatestCommonDivisor(pol4, pol3), res);
+    Polynomial pol5({
+        {Natural(2), Rational(Integer(1), Natural(1))},
+        {Natural(1), Rational(Integer(1), Natural(1))},
+        {Natural(0), Rational(Integer(1), Natural(1))}
+    });
+    Polynomial pol6({
+        {Natural(1), Rational(Integer(-1), Natural(1))},
+    });
+    Polynomial res2({
+        {Natural(0), Rational(Integer(1), Natural(1))},
+    });
+    EXPECT_EQ(Polynomial::GreatestCommonDivisor(pol5, pol6), res2);
+}
