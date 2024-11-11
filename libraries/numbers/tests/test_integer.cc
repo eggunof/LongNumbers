059e089088dd06aa267f15a5f260102396af6463
Darya Dmitrieva <144445820+dmitrievaaxx@users.noreply.github.com>
2024-11-11 21:28:14 +0300

    Z-4

--- libraries/numbers/tests/test_integer.cc
+++ libraries/numbers/tests/test_integer.cc
@@ -7,9 +7,10 @@ TEST(IntegerTest, NaturalToInteger) {
   Natural a("12345678901234567890");
   EXPECT_EQ(Integer(a), Integer("12345678901234567890");
 
-  Natural c("98877665255252626266266262525")
+  Natural c("98877665255252626266266262525");
   EXPECT_EQ(Integer(c), Integer("98877665255252626266266262525");
 }
+
 TEST(IntegerTest, IntegerToNatural) {
   Integer a("8641561246");
   EXPECT_EQ(Natural(a), Natural("8641561246"));
