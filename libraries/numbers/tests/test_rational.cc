3e69d4876b30c8d9fa0748d09ee4c53f0d167a3e
Darya Dmitrieva <144445820+dmitrievaaxx@users.noreply.github.com>
2024-11-12 01:07:59 +0300

    Q-1 with updated tests

--- libraries/numbers/tests/test_rational.cc
+++ libraries/numbers/tests/test_rational.cc
@@ -3,18 +3,20 @@
 
 #include "rational.h"
 
-TEST(RationalTest, Dummy) { EXPECT_TRUE(true); }
-
 TEST(RationalTest, Reduce) {
   Rational a("25/5");
   a.Reduce();
-  EXPECT_TRUE(a, Rational("5/1"));
+  EXPECT_EQ(a, Rational("5/1"));
 
   Rational b("123456789999333/9876543211234");
   b.Reduce();
-  EXPECT_TRUE(a, Rational("17636684285619/1410934744462"));
+  EXPECT_EQ(b, Rational("17636684285619/1410934744462"));
 
   Rational c("1234567891234567/123");
   c.Reduce();
-  EXPECT_TRUE(a, Rational("1234567891234567/123"));
+  EXPECT_EQ(c, Rational("1234567891234567/123"));
+
+  Rational d("-674748493870000/477484848784");
+  d.Reduce();
+  EXPECT_EQ(d, Rational("-42171780866875/29842803049"));
 }
