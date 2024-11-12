23adac3103c0eda861bd4a5a407392066fe5427c
Varfolomeeva Arina <141402789+varfik@users.noreply.github.com>
2024-11-12 16:53:40 +0300

    Update test_rational.cc
    = added

--- libraries/numbers/tests/test_rational.cc
+++ libraries/numbers/tests/test_rational.cc
@@ -79,13 +79,13 @@ TEST(RationalTest, AddDenominators) {
 TEST(RationalTest, Division) {
   Rational a("-9/2");
   Rational b("7/4");
-  EXPECT_EQ(a / b, Rational("-18/7"));
+  EXPECT_EQ(a /= b, Rational("-18/7"));
 
   Rational c("1500000000000000/120000000000001");
   Rational d("50000000000000/24400000000000000");
-  EXPECT_EQ(c / d, Rational("732000000000000000/120000000000001"));
+  EXPECT_EQ(c /= d, Rational("732000000000000000/120000000000001"));
 
   Rational e("-18000000000000000/9000000000000000");
   Rational f("-3000000000000000/200000000000");
-  EXPECT_EQ(e / f, Rational("1/7500"));
+  EXPECT_EQ(e /= f, Rational("1/7500"));
 }
