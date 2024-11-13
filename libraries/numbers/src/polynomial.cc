476a2ca2fac1adc122e4fdb5267f1e6db7827953
Екатерина Солдунова <144446182+ekaterina3383@users.noreply.github.com>
2024-11-13 16:38:11 +0300

    P-9

--- libraries/numbers/src/polynomial.cc
+++ libraries/numbers/src/polynomial.cc
@@ -9,7 +9,13 @@ Polynomial Polynomial::operator*(const Rational &scalar) const { return {}; }
 
 Polynomial Polynomial::operator*(const Polynomial &rhs) const { return {}; }
 
-Polynomial Polynomial::operator/(const Polynomial &rhs) const { return {}; }
+// Частное от деления многочлена на многочлен при делении с остатком
+// Над модулем работала Солдунова Екатерина, гр. 3383
+Polynomial Polynomial::operator/(const Polynomial &rhs) const {
+  Polynomial result = *this;
+  result /= rhs;
+  return result;
+}
 
 Polynomial Polynomial::operator%(const Polynomial &rhs) const { return {}; }
 
@@ -21,7 +27,25 @@ Polynomial &Polynomial::operator*=(const Rational &scalar) { return *this; }
 
 Polynomial &Polynomial::operator*=(const Polynomial &rhs) { return *this; }
 
-Polynomial &Polynomial::operator/=(const Polynomial &rhs) { return *this; }
+// Частное от деления многочлена на многочлен при делении с остатком
+// Над модулем работала Солдунова Екатерина, гр. 3383
+Polynomial &Polynomial::operator/=(const Polynomial &rhs) {
+  Polynomial result;
+  // Остаток от деления
+  Polynomial remainder = *this;
+  // До момента, пока степень делителя меньше или равна степени многочлена, продолжаем деление
+  while (rhs.GetDegree() <= remainder.GetDegree()){
+    remainder.GetLeadingCoefficient();
+    remainder.GetDegree();
+    // Подсчитываем, на сколько нужно увеличить степень делителя для того, чтоюы сравнять её со степенью многочлена 
+    // и для данной степени вычисляем коэффициент, при котором старший член при вычитании сократится
+    result[remainder.GetDegree()-rhs.GetDegree()] = remainder.GetLeadingCoefficient()/rhs.GetLeadingCoefficient();
+    // Вычитаем из многочлена произведение делителя и вышеописанного члена
+    remainder -= (rhs*remainder.GetLeadingCoefficient()/rhs.GetLeadingCoefficient()).MultiplyByPower(remainder.GetDegree()-rhs.GetDegree());
+  }
+  *this = result;
+  return *this;
+}
 
 Polynomial &Polynomial::operator%=(const Polynomial &rhs) { return *this; }
 
