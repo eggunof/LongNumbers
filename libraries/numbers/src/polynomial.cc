a6d45ffd1b17c38614f1dc16c92220fd475afca8
KrivosheinaDaria <144446460+KrivosheinaDaria@users.noreply.github.com>
2024-11-13 13:47:52 +0300

    P-13

--- libraries/numbers/src/polynomial.cc
+++ libraries/numbers/src/polynomial.cc
@@ -38,6 +38,38 @@ Polynomial Polynomial::GreatestCommonDivisor(const Polynomial &first,
 
 Polynomial Polynomial::Derivative(const Polynomial &polynomial) { return {}; }
 
+// NMR_P_P - преобразование многочлена — кратные корни в простые
+// Над модулем работала Кривошеина Дарья, группа 3383
 Polynomial Polynomial::NormalizeRoots(const Polynomial &polynomial) {
-  return {};
+  // создаем объект для будущего нормализованного многочлена (равен 1)
+  Polynomial normalized_polynomial(
+      {{Natural(0), Rational(Integer(1), Natural(1))}});
+  // многочлен, равный переданному, который будет изменяться
+  Polynomial current_polynomial = polynomial;
+  // цикл, пока изменяемый многочлен не станет константой
+  while (current_polynomial.GetDegree() != Natural(0)) {
+    // берется производная от изменяемого многочлена
+    Polynomial derivative = Derivative(current_polynomial);
+    // находится gcd1 - НОД изменяемого многочлена и его производной
+    Polynomial gcd1 = GreatestCommonDivisor(current_polynomial, derivative);
+    // находится interim1 - результат деления изменяемого многочлена на
+    // найденный выше НОД
+    Polynomial interim1 = current_polynomial / gcd1;
+    // находится gcd2 - НОД временного многочлена и производной изменяемого
+    Polynomial gcd2 = GreatestCommonDivisor(interim1, derivative);
+    // находится interim2 - результат деления interim1 на gcd2
+    Polynomial interim2 = interim1 / gcd2;
+    // коэффициенты interim2 сокращаются на (НОД числителей / НОК знаменателей)
+    interim2.ToIntegerCoefficients();
+    // если старший коэффициент interim2 отрицательный, многочлен умножается на
+    // -1
+    if (interim2.GetLeadingCoefficient().GetNumerator().GetSign() ==
+        Sign::NEGATIVE) {
+      interim2 *= Rational("-1");
+    }
+    // нормализованный многочлен умножается на получившийся
+    normalized_polynomial *= interim2;
+    current_polynomial = gcd1;
+  }
+  return normalized_polynomial;
 }
--- libraries/numbers/tests/test_polynomial.cc
+++ libraries/numbers/tests/test_polynomial.cc
@@ -4,3 +4,38 @@
 #include "polynomial.h"
 
 TEST(PolynomialTest, Dummy) { EXPECT_TRUE(true); }
+
+TEST(PolynomialTest, NormalizeRootsTest){
+  Polynomial pol1({{Natural(0), Rational(Integer("-12"), Natural(1))},
+                   {Natural(1), Rational(Integer("-20"), Natural(1))},
+                   {Natural(2), Rational(Integer("5"), Natural(1))},
+                   {Natural(3), Rational(Integer("16"), Natural(1))},
+                   {Natural(4), Rational(Integer("-2"), Natural(1))},
+                   {Natural(5), Rational(Integer("-4"), Natural(1))},
+                   {Natural(6), Rational(Integer("1"), Natural(1))}});
+  pol1 = pol1.NormalizeRoots(pol1);
+  Polynomial pol2({{Natural(0), Rational(Integer("6"), Natural(1))},
+                   {Natural(1), Rational(Integer("1"), Natural(1))},
+                   {Natural(2), Rational(Integer("-4"), Natural(1))},
+                   {Natural(3), Rational(Integer("1"), Natural(1))}});
+  EXPECT_EQ(pol1, pol2);
+
+  Polynomial pol3({{Natural(0), Rational(Integer("25"), Natural(1))},
+                   {Natural(1), Rational(Integer("-1"), Natural(1))},
+                   {Natural(2), Rational(Integer("1"), Natural(1))}});
+  pol3 = pol3.NormalizeRoots(pol3);
+  Polynomial pol4({{Natural(0), Rational(Integer("25"), Natural(1))},
+                   {Natural(1), Rational(Integer("-1"), Natural(1))},
+                   {Natural(2), Rational(Integer("1"), Natural(1))}});
+  EXPECT_EQ(pol3, pol4);
+
+  Polynomial pol5({{Natural(0), Rational(Integer("845"), Natural(1))},
+                   {Natural(1), Rational(Integer("39"), Natural(1))},
+                   {Natural(2), Rational(Integer("-21"), Natural(1))},
+                   {Natural(3), Rational(Integer("1"), Natural(1))}});
+  pol5 = pol5.NormalizeRoots(pol5);
+  Polynomial pol6({{Natural(0), Rational(Integer("-65"), Natural(1))},
+                   {Natural(1), Rational(Integer("-8"), Natural(1))},
+                   {Natural(2), Rational(Integer("1"), Natural(1))}});
+  EXPECT_EQ(pol5, pol6);
+}
