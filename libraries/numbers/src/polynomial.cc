65e6d5bf7caf8d38b062821afdbb74347238400b
eggunof <eggunof@gmail.com>
2024-11-14 20:43:11 +0300

    fixes, tests

--- libraries/numbers/src/polynomial.cc
+++ libraries/numbers/src/polynomial.cc
@@ -1,6 +1,49 @@
 
 #include "polynomial.h"
 
+#include <regex>
+#include <sstream>
+
+Polynomial::Polynomial(std::string string) {
+  std::regex term_regex(
+      R"((((?:[+-]+\s*)?(?:(\d+\s*)(?:\/\s*\d+\s*)?)?)?\*?x(?:\^?\s*(\d+\s*))?)|((?:[+-]+\s*)?\d+\s*(?:\/\s*\d+\s*)?))");
+  std::sregex_iterator term_begin(string.begin(), string.end(), term_regex),
+      term_end;
+
+  for (auto it = term_begin; it != term_end; ++it) {
+    const std::smatch &match = *it;
+
+    Rational coefficient;
+    Natural degree;
+
+    if (match[1].matched) {
+      std::string coefficient_str = match[2].matched ? match[2].str() : "1";
+      if (coefficient_str.find_first_not_of("+- ") == std::string::npos)
+        coefficient_str += "1";
+      coefficient = Rational(coefficient_str);
+      std::string degree_str = match[4].matched ? match[4].str() : "1";
+      degree = Natural(degree_str);
+    } else if (match[5].matched) {
+      coefficient = Rational(match[5].str());
+      degree = Natural("0");
+    }
+
+    coefficients_[degree] += coefficient;
+
+    if (coefficients_[degree] == Rational("0")) {
+      coefficients_.erase(degree);
+    }
+  }
+}
+
+bool Polynomial::operator==(const Polynomial &rhs) const {
+  return coefficients_ == rhs.coefficients_;
+}
+
+bool Polynomial::operator!=(const Polynomial &rhs) const {
+  return !(rhs == *this);
+}
+
 // Сложение многочленов "+"
 // Над модулем работала Варфоломеева Арина, гр. 3383
 Polynomial Polynomial::operator+(const Polynomial &rhs) const {
@@ -59,4 +102,30 @@ Polynomial Polynomial::Derivative(const Polynomial &polynomial) { return {}; }
 
 Polynomial Polynomial::NormalizeRoots(const Polynomial &polynomial) {
   return {};
+}
+
+Polynomial::operator std::string() const {
+  std::ostringstream result;
+  bool first_monomial = true;
+  for (auto &[degree, coefficient] : coefficients_) {
+    if (coefficient != Rational("1") && coefficient != Rational("-1")) {
+      if (!first_monomial && coefficient > Rational("0")) {
+        result << "+";
+      }
+      result << std::string(coefficient);
+      if (!degree.IsZero()) result << "*";
+    } else if (coefficient == Rational("-1")) {
+      result << "-";
+      if (degree.IsZero()) result << "1";
+    }
+    if (!degree.IsZero()) {
+      result << "x";
+      if (degree != Natural("1")) {
+        result << "^" << std::string(degree);
+      }
+    }
+    first_monomial = false;
+  }
+  if (result.tellp() == 0) return "0";
+  return result.str();
 }
--- libraries/numbers/tests/test_polynomial.cc
+++ libraries/numbers/tests/test_polynomial.cc
@@ -3,7 +3,69 @@
 
 #include "polynomial.h"
 
-TEST(PolynomialTest, Dummy) { EXPECT_TRUE(true); }
+TEST(PolynomialTest, StringToPolynomial) {
+  Polynomial a("3/2*x^5 - 4*x^3 + 1/2*x - 2");
+  auto a_map = std::map<Natural, Rational, Comparator>{
+      {Natural("5"), Rational("3/2")},
+      {Natural("3"), Rational("-4")},
+      {Natural("1"), Rational("1/2")},
+      {Natural("0"), Rational("-2")},
+  };
+  EXPECT_EQ(a, Polynomial(a_map));
+
+  Polynomial b("5/3*x^4 + 7*x + 2");
+  auto b_map = std::map<Natural, Rational, Comparator>{
+      {Natural("4"), Rational("5/3")},
+      {Natural("1"), Rational("7")},
+      {Natural("0"), Rational("2")},
+  };
+  EXPECT_EQ(b, Polynomial(b_map));
+
+  Polynomial c(" -3*x^4  + 6/5*x^2 - x +   1/3");
+  auto c_map = std::map<Natural, Rational, Comparator>{
+      {Natural("4"), Rational("-3")},
+      {Natural("2"), Rational("6/5")},
+      {Natural("1"), Rational("-1")},
+      {Natural("0"), Rational("1/3")},
+  };
+  EXPECT_EQ(c, Polynomial(c_map));
+
+  Polynomial d("4*x^7 + 5 - x^5 + 1/2*x^2 - 7/8");
+  auto d_map = std::map<Natural, Rational, Comparator>{
+      {Natural("7"), Rational("4")},
+      {Natural("5"), Rational("-1")},
+      {Natural("2"), Rational("1/2")},
+      {Natural("0"), Rational("33/8")},
+  };
+  EXPECT_EQ(d, Polynomial(d_map));
+
+  Polynomial e("2*x^3 - 3*x^3 + x + x - 1");
+  auto e_map = std::map<Natural, Rational, Comparator>{
+      {Natural("3"), Rational("-1")},
+      {Natural("1"), Rational("2")},
+      {Natural("0"), Rational("-1")},
+  };
+  EXPECT_EQ(e, Polynomial(e_map));
+
+  Polynomial f("3*x^3 - 3*x^3 + x - x + 2 - 2");
+  auto f_map = std::map<Natural, Rational, Comparator>{};
+  EXPECT_EQ(f, Polynomial(f_map));
+
+  Polynomial g(" 7 x^3 + 1 / 2 x  - 3 ");
+  auto g_map = std::map<Natural, Rational, Comparator>{
+      {Natural("3"), Rational("7")},
+      {Natural("1"), Rational("1/2")},
+      {Natural("0"), Rational("-3")},
+  };
+  EXPECT_EQ(g, Polynomial(g_map));
+
+  Polynomial h("x^5 - x^2");
+  auto h_map = std::map<Natural, Rational, Comparator>{
+      {Natural("5"), Rational("1")},
+      {Natural("2"), Rational("-1")},
+  };
+  EXPECT_EQ(h, Polynomial(h_map));
+}
 
 TEST(PolynomialTest, Addition) {
   Polynomial pol1({{Natural(0), Rational(Integer(1), Natural(1))},
