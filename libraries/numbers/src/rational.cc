6fb3f29414916be1b3c76db6adb495a204342fb9
eggunof <eggunof@gmail.com>
2024-11-16 11:28:10 +0300

    refactor Rational

--- libraries/numbers/src/rational.cc
+++ libraries/numbers/src/rational.cc
@@ -3,70 +3,63 @@
 
 #include <sstream>
 #include <stdexcept>
+#include <utility>
 
 Rational::Rational() : numerator_(0), denominator_(1) {}
 
-Rational::Rational(const Integer &numerator, const Natural &denominator)
-    : numerator_(numerator), denominator_(denominator) {
+Rational::Rational(Integer numerator, Natural denominator)
+    : numerator_(std::move(numerator)), denominator_(std::move(denominator)) {
   if (denominator_.IsZero()) {
     throw std::invalid_argument("Invalid input: denominator cannot be zero");
   }
 }
+
+Rational::Rational(Integer integer)
+    : numerator_(std::move(integer)), denominator_(1) {}
+
+Rational::Rational(Natural natural)
+    : numerator_(std::move(natural)), denominator_(1) {}
 
 Rational::Rational(const std::string &string) {
-  size_t pos = string.find('/');
-  if (pos != std::string::npos) {
-    numerator_ = Integer(string.substr(0, pos));
-    denominator_ = Natural(string.substr(pos + 1));
-  } else {
-    numerator_ = Integer(string);
-    denominator_ = Natural("1");
-  }
-  if (denominator_.IsZero()) {
-    throw std::invalid_argument("Invalid input: denominator cannot be zero");
-  }
+  std::stringstream ss;
+  ss << string;
+  ss >> *this;
 }
 
-// Преобразование целого в дробное
-// Над модулем работала Кривошеина Дарья, гр. 3383
-Rational::Rational(const Integer &integer)
-    : numerator_(integer), denominator_(Natural("1")) {}
-
-// Преобразование дробного в целое
-// Над модулем работал Егунов Даниил, гр. 3383
 Rational::operator Integer() const {
   Integer denominator = static_cast<Integer>(denominator_);
   // Если не делится нацело, выбрасываем исключение
   if (numerator_ % denominator != Integer("0")) {
-    throw std::invalid_argument(
-        "Invalid input: Rational cannot be converted to an Integer without a "
-        "remainder");
+    throw std::logic_error(
+        "Rational cannot be converted to an Integer without a remainder");
   }
   // Если делится нацело, возвращаем целое частное
   return numerator_ / denominator;
 }
 
-// Сокращение дроби
-// Над модулем работала Дмитриева Дарья, гр. 3383
+bool Rational::IsInteger() {
+  // Сокращаем
+  Reduce();
+  // Сокращённое дробное является целым, если знаменатель равен 1
+  return denominator_ == Natural(1);
+}
+
 Rational &Rational::Reduce() {
   // Находим НОД числителя и знаменателя
   Natural gcd = Natural::GreatestCommonDivisor(
       static_cast<Natural>(Integer::AbsoluteValue(numerator_)), denominator_);
   // Если НОД равен единице, дробь уже сокращена
-  if (gcd == Natural("1")) return *this;
+  if (gcd == Natural(1)) return *this;
   // Сокращаем дробь
   numerator_ /= Integer(gcd);
   denominator_ /= gcd;
   return *this;
 }
 
-// Проверка сокращённого дробного на целое
-// Над модулем работал Матвеев Никита, гр. 3383
-bool Rational::IsInteger() {
-  // Сокращаем
-  Reduce();
-  // Сокращённое дробное является целым, если знаменатель равен 1
-  return denominator_ == Natural("1");
+Rational Rational::Reduce() const {
+  Rational result = *this;
+  result.Reduce();
+  return result;
 }
 
 const Integer &Rational::GetNumerator() const { return numerator_; }
@@ -99,8 +92,11 @@ Rational &Rational::operator-() {
   return *this;
 }
 
-// Сложение дробей "+="
-// Над модулем работала Майская Вероника, гр. 3384
+Rational Rational::operator-() const {
+  Rational result = *this;
+  return -result;
+}
+
 Rational &Rational::operator+=(const Rational &rhs) {
   // Находим НОК знаменателей
   Natural lcm = Natural::LeastCommonMultiple(denominator_, rhs.denominator_);
@@ -118,16 +114,26 @@ Rational &Rational::operator+=(const Rational &rhs) {
   return *this;
 }
 
-// Вычитание дробей "-="
-// Над модулем работала Кадникова Анна, гр. 3384
+Rational Rational::operator+(const Rational &rhs) const {
+  // Складываем копию текущего объекта
+  Rational result = *this;
+  result += rhs;
+  return result;
+}
+
 Rational &Rational::operator-=(const Rational &rhs) {
   // Применяем сложение с противоположным знаком
   *this += -rhs;
   return *this;
 }
 
-// Умножение дробей "*="
-// Над модулем работала Солдунова Екатерина, гр. 3383
+Rational Rational::operator-(const Rational &rhs) const {
+  // Вычитаем копию текущего объекта
+  Rational result = *this;
+  result -= rhs;
+  return result;
+}
+
 Rational &Rational::operator*=(const Rational &rhs) {
   numerator_ *= rhs.numerator_;
   denominator_ *= rhs.denominator_;
@@ -136,8 +142,12 @@ Rational &Rational::operator*=(const Rational &rhs) {
   return *this;
 }
 
-// Деление дробей "/="
-// Над модулем работала Варфоломеева Арина, гр. 3383
+Rational Rational::operator*(const Rational &rhs) const {
+  Rational result = *this;
+  result *= rhs;
+  return result;
+}
+
 Rational &Rational::operator/=(const Rational &rhs) {
   if (rhs.numerator_ == Integer("0")) {
     throw std::invalid_argument(
@@ -150,50 +160,36 @@ Rational &Rational::operator/=(const Rational &rhs) {
   return *this;
 }
 
-Rational Rational::operator-() const {
-  Rational result = *this;
-  return -result;
-}
-
-// Сложение дробей "+"
-// Над модулем работала Майская Вероника, гр. 3384
-Rational Rational::operator+(const Rational &rhs) const {
-  // Складываем копию текущего объекта
-  Rational result = *this;
-  result += rhs;
-  return result;
-}
-
-// Вычитание дробей "-"
-// Над модулем работала Кадникова Анна, гр. 3384
-Rational Rational::operator-(const Rational &rhs) const {
-  // Вычитаем копию текущего объекта
-  Rational result = *this;
-  result -= rhs;
-  return result;
-}
-
-// Умножение дробей "*"
-// Над модулем работала Солдунова Екатерина, гр. 3383
-Rational Rational::operator*(const Rational &rhs) const {
-  Rational result = *this;
-  result *= rhs;
-  return result;
-}
-
-// Деление дробей "/"
-// Над модулем работала Варфоломеева Арина, гр. 3383
 Rational Rational::operator/(const Rational &rhs) const {
   Rational result = *this;
   result /= rhs;
   return result;
 }
 
-Rational::operator std::string() const {
-  std::ostringstream result;
-  result << std::string(numerator_);
-  if (denominator_ != Natural("1")) {
-    result << "/" << std::string(denominator_);
+std::istream &operator>>(std::istream &is, Rational &rational) {
+  is >> rational.numerator_ >> std::ws;
+  if (is.good() && is.peek() == '/') {
+    is.get();
+    is >> rational.denominator_;
+  } else {
+    rational.denominator_ = Natural(1);
   }
+  if (rational.denominator_.IsZero()) {
+    throw std::invalid_argument("Invalid input: denominator cannot be zero");
+  }
+  return is;
+}
+
+std::ostream &operator<<(std::ostream &os, const Rational &rational) {
+  os << rational.numerator_;
+  if (rational.denominator_ != Natural(1)) {
+    os << "/" << rational.denominator_;
+  }
+  return os;
+}
+
+std::string Rational::ToString() const {
+  std::ostringstream result;
+  result << *this;
   return result.str();
 }
